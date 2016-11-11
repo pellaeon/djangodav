f9cfab481974fddacc0ea1c3b2bc34a7b526b609
shunyi <M157q.tw@gmail.com>
2016-11-11 23:22:10 +0800

    Fix djangodav/testapp/settings for Django.
    Should not add ',' after string, it will be converted to tuple.

--- djangodav/testapp/settings.py
+++ djangodav/testapp/settings.py
@@ -1,23 +1,25 @@
 import random
 import string
 
-from django.conf import settings
+from tempfile import gettempdir
+
 
 INSTALLED_APPS = (
-        'djangodav',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        # 'djangodav.tests',
-        ),
-DATABASES = dict(
-        default = dict(
-            ENGINE = 'django.db.backends.sqlite3'
-            )
-        ),
-ROOT_URLCONF = 'djangodav.tests.urls',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+
+    'djangodav',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+
+ROOT_URLCONF = 'djangodav.tests.urls'
 MIDDLEWARE_CLASSES = ()
 
-from tempfile import gettempdir
 WEBDAV_ROOT = gettempdir()
 
 SECRET_KEY = ''.join([random.choice(string.ascii_letters) for x in range(40)])
